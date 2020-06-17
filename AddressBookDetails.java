96e361df8ad95d3a831229f2d7a3806a5c337250
“Aniket_D” <aniketdakhare@gmail.com>
2020-06-17 18:08:53 +0530

    MODIFIED code and ADDAD provision to add a new person to Address Book

--- AddressBookDetails.java
+++ AddressBookDetails.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class AddressBookDetails
 {
     //Variables
@@ -8,6 +10,46 @@ public class AddressBookDetails
     String state;
     int zipCode;
     String phoneNumber;
+    Scanner scan=new Scanner(System.in);
+
+    //Method to take Name from User
+    public void addName()
+    {
+        System.out.println("Enter your First Name");
+        this.firstName = scan.nextLine();
+        System.out.println("Enter your Last Name");
+        this.lastName = scan.nextLine();
+        addDetails();
+    }
+
+    //Method to take Name from User
+    public void addDetails()
+    {
+        scan.nextLine();
+        System.out.println("Enter your Address");
+        this.address = scan.nextLine();
+        System.out.println("Enter your City");
+        this.city = scan.nextLine();
+        System.out.println("Enter your State");
+        this.state = scan.nextLine();
+        System.out.println("Enter your Zip code");
+        this.zipCode = scan.nextInt();
+        int length = (int)(Math.log10(this.zipCode)+1);
+        while(length != 6)
+        {
+            System.out.println("enter 6 digit number");
+            this.zipCode = scan.nextInt();
+            length = (int)(Math.log10(this.zipCode)+1);
+            if(length==6)
+            {
+                break;
+            }
+        }
+        System.out.println("Enter your Phone Number");
+        this.phoneNumber = scan.next();
+    }
+
+
 
     //Method to Display Address Book Details
     public void display()
@@ -17,15 +59,4 @@ public class AddressBookDetails
         System.out.println("NAME: "+this.firstName+" "+this.lastName+"  "+"ADDRESS: "+this.address+"  "+"CITY: "+this.city+"  "+"STATE: "+this.state+"  "+"ZIPCODE: "+this.zipCode+"  "+"PHONE NO.: "+this.phoneNumber);
     }
 
-    //Constructor to initialize variables
-    AddressBookDetails(String firstName, String lastName, String address, String city, String state, int zipCode,String phoneNumber)
-    {
-        this.firstName=firstName;
-        this.lastName=lastName;
-        this.address=address;
-        this.city=city;
-        this.state=state;
-        this.zipCode=zipCode;
-        this.phoneNumber=phoneNumber;
-    }
 }
--- AddressBookMain.java
+++ AddressBookMain.java
@@ -1,10 +1,10 @@
 public class AddressBookMain
 {
-	 //Main Method
     public static void main(String[] args)
     {
         System.out.println("Welcome to Address Book Program");
-        AddressBookDetails addressBookDetails=new AddressBookDetails("Aniket", "Dakhare","Dhanori", "Pune","Maharashtra",442502,"9978659035");
+        AddressBookDetails addressBookDetails=new AddressBookDetails();
+        addressBookDetails.addName();
         addressBookDetails.display();
     }
 }
