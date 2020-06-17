9c1216754b25b06973c14cdbc3361f19d8d4e2f2
“Aniket_D” <aniketdakhare@gmail.com>
2020-06-17 21:28:42 +0530

    MODIFIED code and ADDED provision to add multiple person details in Address Book

--- AddressBookDetails.java
+++ AddressBookDetails.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class AddressBookDetails
@@ -11,68 +13,77 @@ public class AddressBookDetails
     int zipCode;
     String phoneNumber;
     Scanner scan=new Scanner(System.in);
+    List<AddressBookDetails> addressBookList= new ArrayList<AddressBookDetails>();
 
     //Method to take Name from User
     public void addName()
     {
+        AddressBookDetails addressBook = new AddressBookDetails();
         System.out.println("Enter your First Name");
-        this.firstName = scan.nextLine();
+        addressBook.firstName = scan.next();
         System.out.println("Enter your Last Name");
-        this.lastName = scan.nextLine();
-        addDetails();
+        addressBook.lastName = scan.next();
+        addDetails(addressBook);
+        addressBookList.add(addressBook);
     }
 
     //Method to take Name from User
-    public void addDetails()
+    public void addDetails(AddressBookDetails addressBook)
     {
         scan.nextLine();
         System.out.println("Enter your Address");
-        this.address = scan.nextLine();
+        addressBook.address = scan.nextLine();
         System.out.println("Enter your City");
-        this.city = scan.nextLine();
+        addressBook.city = scan.nextLine();
         System.out.println("Enter your State");
-        this.state = scan.nextLine();
+        addressBook.state = scan.nextLine();
         System.out.println("Enter your Zip code");
-        this.zipCode = scan.nextInt();
-        int length = (int)(Math.log10(this.zipCode)+1);
+        addressBook.zipCode = scan.nextInt();
+        int length = (int)(Math.log10(addressBook.zipCode)+1);
         while(length != 6)
         {
             System.out.println("enter 6 digit number");
-            this.zipCode = scan.nextInt();
-            length = (int)(Math.log10(this.zipCode)+1);
+            addressBook.zipCode = scan.nextInt();
+            length = (int)(Math.log10(addressBook.zipCode)+1);
             if(length==6)
             {
                 break;
             }
         }
         System.out.println("Enter your Phone Number");
-        this.phoneNumber = scan.next();
+        addressBook.phoneNumber = scan.next();
     }
 
     //Method to Edit and Delete record from Address Book
-    public void editOrDeleteDetails(int check)
+    public void editOrDeleteDetails(int select)
     {
+        boolean check = true;
         System.out.println("Enter your First name");
         String firstName = scan.next();
         System.out.println("Enter your Last name");
         String lastName = scan.next();
-        if( this.firstName.equals(firstName) )
+        for ( AddressBookDetails details : addressBookList)
         {
-            if(this.lastName.equals(lastName))
+            if( details.firstName.equals(firstName) )
             {
-                switch (check)
+                if(details.lastName.equals(lastName))
                 {
-                    case 0:
-                        addDetails();
-                        break;
-                    case 1:
-                        break;
+                    check=false;
+                    switch (select)
+                    {
+                        case 0:
+                            addDetails(details);
+                            break;
+                        case 1:
+                            addressBookList.remove(details);
+                            break;
+                    }
                 }
             }
-            else
-            {
-                System.out.println("Record does not exist");
-            }
+        }
+        if (check==true)
+        {
+            System.out.println("Record does not exist");
         }
     }
 
@@ -81,7 +92,11 @@ public class AddressBookDetails
     {
         System.out.println("ADDRESS BOOK DETAILS : ");
         System.out.println("-----------------------------------------------------------------------------------------------------");
-        System.out.println("NAME: "+this.firstName+" "+this.lastName+"  "+"ADDRESS: "+this.address+"  "+"CITY: "+this.city+"  "+"STATE: "+this.state+"  "+"ZIPCODE: "+this.zipCode+"  "+"PHONE NO.: "+this.phoneNumber);
+        for ( AddressBookDetails details : addressBookList)
+        {
+            System.out.println("NAME: "+details.firstName+" "+details.lastName+"  "+"ADDRESS: "+details.address+"  "+"CITY: "+details.city+"  "+"STATE: "+details.state+"  "+"ZIPCODE: "+details.zipCode+"  "+"PHONE NO.: "+details.phoneNumber);
+
+        }
     }
 
 }
